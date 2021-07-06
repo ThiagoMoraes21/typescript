e8dc86ee94344aadf6da3c14c45fbc6f6167fb40
Thiago Moraes Oliveira <t.moraes.oliveira@avanade.com>
2021-07-06 17:33:35 -0300

    add controller methods, typed old methods and variables

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,9 @@
-import { Negociacao } from "./models/negociacao.js";
+import { NegociacaoController } from "./controllers/negociacao.controller.js";
 
-const negociacao = new Negociacao(new Date(), 10, 100);
-console.log(negociacao);
-console.log(negociacao.data);
-console.log(negociacao.volume);
+const negociacaoController = new NegociacaoController();
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    negociacaoController.adiciona();
+});
