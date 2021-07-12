79bd48f943e0131181a9cfe55210bae566ef3a76
Thiago Moraes <thiago4x70@gmail.com>
2021-07-12 14:32:35 -0300

    add service to get external data to fetch the table, add a new interface to model the api returned data

--- app/dist/js/app.js
+++ app/dist/js/app.js
@@ -10,3 +10,12 @@ if (form) {
 else {
     throw Error('Não foi possível inicializar a aplicação. Verifique se o form existe!');
 }
+const btnImporta = document.querySelector('#botao-importa');
+if (btnImporta) {
+    btnImporta.addEventListener('click', () => {
+        negociacaoController.importaDados();
+    });
+}
+else {
+    throw Error('Botão importa não foi encontrado');
+}
