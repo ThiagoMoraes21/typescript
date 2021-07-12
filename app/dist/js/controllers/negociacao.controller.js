5ccefc89d75508cb0e63f6c81151be3ef79556f4
Thiago Moraes <thiago4x70@gmail.com>
2021-07-12 18:56:14 -0300

    add a generic interface to compare any comparable object

--- app/dist/js/controllers/negociacao.controller.js
+++ app/dist/js/controllers/negociacao.controller.js
@@ -34,7 +34,15 @@ export class NegociacaoController {
         this.limparFormulario();
     }
     importaDados() {
-        this.negociacoesService.obterNegociacoesDoDia()
+        this.negociacoesService
+            .obterNegociacoesDoDia()
+            .then(negociacoesDeHoje => {
+            return negociacoesDeHoje.filter(negociacaoDeHoje => {
+                return !this.negociacoes
+                    .lista()
+                    .some(negociacao => negociacao.ehIgual(negociacaoDeHoje));
+            });
+        })
             .then(negociacoesDeHoje => {
             for (let negociacao of negociacoesDeHoje) {
                 this.negociacoes.adiciona(negociacao);
